c7a102233768a48fa72e1ad81a8b014724b1b509
Yanni Fraga <yanni.fraga@acrontum.de>
2024-07-29 23:27:18 +0200

    Started working on video carousel component

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,6 +1,7 @@
 import {useGSAP} from '@gsap/react';
 import gsap from 'gsap';
 import {rightImg, watchImg} from '../utils/index.js';
+import VideoCarousel from './VideoCarousel.jsx';
 
 const Highlights = () => {
   useGSAP(() => {
@@ -26,6 +27,7 @@ const Highlights = () => {
           </div>
         </div>
 
+        <VideoCarousel />
       </div>
     </section>
   )
